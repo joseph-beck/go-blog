6331b7a603b4f3a7fab64d64e2a0c2dcbe21d11d
Joseph Beck <joebeck03@icloud.com>
2024-02-03 17:00:44 +0000

    feat: start basic models

--- api/internal/models/user.go
+++ api/internal/models/user.go
@@ -1,4 +1,7 @@
 package models
 
 type User struct {
+	Model
+	Username string `gorm:"type:varchar(25);unique;not null" json:"username"`
+	Email    string `gorm:"type:varchar(100);unique;not null;unique_index" json:"email"`
 }
