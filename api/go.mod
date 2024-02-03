2e1a911df677b9992b7a1c48cceb7e5d6286b258
Joseph Beck <joebeck03@icloud.com>
2024-02-03 21:52:26 +0000

    chore: comment database functions

--- api/go.mod
+++ api/go.mod
@@ -2,12 +2,13 @@ module github.com/joseph-beck/chit-chat/api
 
 go 1.21
 
-require github.com/stretchr/testify v1.8.4
+require (
+	github.com/stretchr/testify v1.8.4
+	github.com/swaggo/swag v1.16.3
+)
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
-	github.com/PuerkitoBio/purell v1.1.1 // indirect
-	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
@@ -30,7 +31,6 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
-	github.com/kr/text v0.2.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -40,13 +40,11 @@ require (
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
-	github.com/swaggo/swag v1.16.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.11.3 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/tools v0.7.0 // indirect
--- api/internal/database/database.go
+++ api/internal/database/database.go
@@ -8,12 +8,14 @@ type Store struct {
 	DB *gorm.DB
 }
 
+// Create a new data Store
 func New(c Conn) *Store {
 	return &Store{
 		DB: c(),
 	}
 }
 
+// Close the data Store
 func (s *Store) Close() error {
 	db, err := s.DB.DB()
 	if err != nil {
@@ -28,6 +30,7 @@ func (s *Store) Close() error {
 	return nil
 }
 
+// Ping the data store
 func (s *Store) Ping() error {
 	db, err := s.DB.DB()
 	if err != nil {
@@ -42,8 +45,10 @@ func (s *Store) Ping() error {
 	return nil
 }
 
-func (s *Store) List(i interface{}, t string) error {
-	res := s.DB.Table(t).Find(i)
+// Get a list of given models from the given table, use a reference for i.
+// Queries can be customized with the p, for example "id = ?", 1
+func (s *Store) List(i interface{}, t string, p ...interface{}) error {
+	res := s.DB.Table(t).Find(i, p...)
 	if res.Error != nil {
 		return res.Error
 	}
@@ -51,6 +56,8 @@ func (s *Store) List(i interface{}, t string) error {
 	return nil
 }
 
+// Get a single instance of a given model from the given table, use a reference for i.
+// Queries can be customized with p, for example "id = ?", 1
 func (s *Store) Get(i interface{}, t string, p ...interface{}) error {
 	res := s.DB.Table(t).Find(i, p...).First(i)
 	if res.Error != nil {
@@ -61,6 +68,7 @@ func (s *Store) Get(i interface{}, t string, p ...interface{}) error {
 
 }
 
+// Create a given model in the given table, use a reference for i
 func (s *Store) Create(i interface{}, t string) error {
 	res := s.DB.Table(t).Create(i)
 	if res.Error != nil {
@@ -70,6 +78,7 @@ func (s *Store) Create(i interface{}, t string) error {
 	return nil
 }
 
+// Update a given model in the given table, use a reference for i
 func (s *Store) Update(i interface{}, t string) error {
 	res := s.DB.Table(t).Save(i)
 	if res.Error != nil {
@@ -79,6 +88,8 @@ func (s *Store) Update(i interface{}, t string) error {
 	return nil
 }
 
+// Delete a given model in the given table, use a reference for i.
+// Queries can be customized with p, for example "id = ?", 1
 func (s *Store) Delete(i interface{}, t string, p ...interface{}) error {
 	res := s.DB.Table(t).Delete(i, p...)
 	if res.Error != nil {
@@ -88,6 +99,8 @@ func (s *Store) Delete(i interface{}, t string, p ...interface{}) error {
 	return nil
 }
 
+// Check if the database contains the given model in the given table, use a reference for i.
+// Queries can be customized with p, for example "id = ?", 1
 func (s *Store) Contains(i interface{}, t string, p ...interface{}) (bool, error) {
 	cnt := int64(0)
 	res := s.DB.Table(t).Find(&i, p...).Count(&cnt)
@@ -98,6 +111,7 @@ func (s *Store) Contains(i interface{}, t string, p ...interface{}) (bool, error
 	return cnt != 0, nil
 }
 
+// Automatically migrate all of the given models
 func (s *Store) AutoMigrate(i ...interface{}) error {
 	if len(i) <= 0 {
 		return nil
@@ -113,16 +127,19 @@ func (s *Store) AutoMigrate(i ...interface{}) error {
 	return nil
 }
 
+// Create a table of the given model
 func (s *Store) CreateTable(m interface{}) error {
 	err := s.DB.Migrator().CreateTable(m)
 	return err
 }
 
+// Drop the tabe of a given name
 func (s *Store) DropTable(t string) error {
 	err := s.DB.Migrator().DropTable(t)
 	return err
 }
 
+// Check if a table of the given name exists
 func (s *Store) HasTable(t string) bool {
 	e := s.DB.Migrator().HasTable(t)
 	return e
