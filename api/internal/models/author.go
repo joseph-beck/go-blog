3a2239671245dc9b3233876081f0606b85f3baab
Joseph Beck <joebeck03@icloud.com>
2024-02-08 22:04:20 +0000

    fix: role in correct place

--- api/internal/models/author.go
+++ api/internal/models/author.go
@@ -5,7 +5,6 @@ type Author struct {
 	Username string `gorm:"type:varchar(25);unique;not null" json:"username"`              // username of the author, must be unique
 	Name     string `gorm:"type:varchar(25);not null" json:"name"`                         // name of the author, can be a duplicate name
 	State    State  `gorm:"type:integer;check:(State >= 0) and (State <= 4)" json:"state"` // state of the user, are they online etc?
-	Role     Role   `gorm:"type:integer;check:(Role >= 0) and (Role <= 3)" json:"role"`    // role of the user, used for user access levels
 	Status   string `gorm:"type:varchar(50)" json:"status"`                                // the current status of the user, a small message
 	About    string `gorm:"type:varchar(255)" json:"about"`                                // about the author, a larger body of text
 	Picture  string `gorm:"type:text" json:"picture"`                                      // link to the image of the author
--- api/internal/models/user.go
+++ api/internal/models/user.go
@@ -27,4 +27,5 @@ type Credentials struct {
 	Name     string `gorm:"type:varchar(25);not null" json:"name"`                       // name of the user, can be a duplicate name
 	Email    Email  `gorm:"type:varchar(100);unique;not null;unique_index" json:"email"` // email of user, may require more validation
 	Password string `gorm:"type:varchar(64);not null" json:"password"`                   // stored with a SHA256
+	Role     Role   `gorm:"type:integer;check:(Role >= 0) and (Role <= 3)" json:"role"`  // role of the user, used for user access levels
 }
